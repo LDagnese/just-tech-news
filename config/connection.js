1ffdd9c6c31e45c65eae8d867b6aa7805feef724
loudagnese <louis.dagnese@gmail.com>
2022-08-02 19:01:42 -0700

    fix heroku connection

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 // Create connection to the database
 let sequelize;
 if (process.env.JAWSDB_URL) {
-    sequelize = Sequelize(process.env.JAWSDB_URL);
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
         host: 'localhost',
